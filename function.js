3721065c34083b6e82cb5e96c6e8e319176e321d
Orée <83070309+Enoryel@users.noreply.github.com>
2023-05-10 00:38:04 +0200

    Génération player v.1

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 var compteur_joueurs = 2;
-var compteur_joueurs_old = null ;
+var compteur_joueurs_old = null;
 
-
+//MODIFICATEUR DU NOMBRE DE JOUEUR·EUSE·S
 function playerSelector() {
     compteur_joueurs_old = compteur_joueurs;
     compteur_joueurs = document.getElementById('player_nbr').value;
@@ -23,14 +23,61 @@ function playerSelector() {
 
 };
 
-function addPlayer() {
-   
-    let playersToAdd = compteur_joueurs - compteur_joueurs_old;
-    console.log (playersToAdd); 
-    console.log("J'ajoute " + playersToAdd + " joueurs");
+//FONCTIONS AJOUTS DE JOUEURS·EUSE·S
+
+function addPlayer() {      
+    while (compteur_joueurs_old < compteur_joueurs){
+        compteur_joueurs_old++;
+        generateNewPlayer();
+        console.log("J'ajoute le joueur " + compteur_joueurs_old);
+    };
 }
 
-function removePlayer() {
-    let playersToRemove = (compteur_joueurs_old - compteur_joueurs);
-    console.log("J'enlève " + playersToRemove + " joueurs");
+function generateNewPlayer() {
+    //Définition des variables de base
+    var newplayerID = "player" + compteur_joueurs_old;
+    var copiedPlayer = "player" + (compteur_joueurs_old - 1);
+    var elem;
+    var clone;
+
+    //Création et incorporation de la nouvelle div
+    var player_chara = document.getElementById("player_chara")
+    var newplayerDiv = document.createElement("div");
+    newplayerDiv.id = newplayerID;
+    player_chara.appendChild(newplayerDiv);
+
+
+    //Création et incorporation du nouvel input de nom
+    var newplayerName = newplayerID + "_name";
+    var newplayerNameValue = "Joueur " + newplayerID;
+    var copiedPlayerName = copiedPlayer + "_name";
+
+    elem = document.getElementById(copiedPlayerName);
+    /*console.log(copiedPlayerName);
+    return;*/
+    clone = elem.cloneNode(true);
+    clone.name = newplayerName;
+    clone.value = newplayerNameValue;
+    newplayerDiv.appendChild(clone);
+
+    //Création et incorporation du nouveau sélecteur de race
+    var newplayerRace = newplayerID + "_race";
+    elem = document.getElementById(copiedPlayer + "_race");
+    clone = elem.cloneNode(true);
+    clone.name = newplayerRace;
+    newplayerDiv.appendChild(clone);
 }
+
+//FONCTIONS SUPPRESSION DE JOUEURS·EUSE·S
+
+function removePlayer() {
+    while (compteur_joueurs_old > compteur_joueurs){  
+        removePlayerDiv();
+        console.log("Je supprime le joueur " + compteur_joueurs_old);
+        compteur_joueurs_old--;
+    };
+}
+
+function removePlayerDiv() {
+
+};
--- index.php
+++ index.php
@@ -2,62 +2,81 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>LOR Helper</title>
+    <title>LoRTool</title>
 
     <script src="function.js" defer></script>
   </head>
 
   <body>
-
-
-<!--NOMBRE DE JOUEUR·EUSE·S-->
-<select name="player_nbr" id="player_nbr" onchange="playerSelector()">
-<?php
-  // Boucle pour générer le nombre de joueur·euse·s
-
-  $nbr_joueur_min = 2;
-  $nbr_joueur_max = 8;
-  for ($i = $nbr_joueur_min; $i <= $nbr_joueur_max; $i++) {
-    echo "<option value='" . $i . "'>" . $i . "</option>";
-  }
-  ?>
-
-<!--RÉCUPÉRATION DEPUIS LA BASE DE DONNÉES-->
+    <h1>LoRTool</h1>
+<!--RÉCUPÉRATION DES RACES DEPUIS LA BASE DE DONNÉES-->
     <?php
-    $ser="localhost:3306";
-    $user="root";
-    $pass="";
-    $db="test";
-    $conn = mysqli_connect($ser ,$user ,$pass ,$db) or die("Connection Failed");
+      $ser="localhost:3306";
+      $user="root";
+      $pass="";
+      $db="test";
+      $conn = mysqli_connect($ser ,$user ,$pass ,$db) or die("Connection Failed");
 
-    $conn->query("SET NAMES 'utf8'"); 
-    $conn->query("SET CHARACTER SET utf8");  
-    $conn->query("SET SESSION collation_connection = 'utf8_unicode_ci'");
+      $conn->query("SET NAMES 'utf8'"); 
+      $conn->query("SET CHARACTER SET utf8");  
+      $conn->query("SET SESSION collation_connection = 'utf8_unicode_ci'");
 
-    $resultat = mysqli_query($conn, "SELECT 'NOM' FROM races");
+      $requete = "SELECT * FROM races_list";
+      $resultat = mysqli_query($conn, $requete);
 
-    $liste_races = [];
-    $compteur = 0;
-    while ($donnees = mysqli_fetch_array($resultat)) {
-      $liste_races[$compteur] = $donnees['NOM'] . ' ' . $donnees['PRENOM'];
-      $compteur ++;
-    }
-
-    mysqli_close($conn);
-    ?>
-
-<!--GÉNÉRATION DU MENU DÉROULANT
-    <select name="mySelect1" id="mySelect1" onchange="player_selector()">
-    <?php
-      foreach($liste_races as $option) {
-        echo '<option value="' . $option . '">' . $option . '</option>';
+      $liste_races = [];
+      $compteur = 0;
+      while ($donnees = mysqli_fetch_array($resultat)) {
+        $liste_races[$compteur] = $donnees["NOM"];
+        $compteur ++;
       }
+
+      mysqli_close($conn);
     ?>
-    </select>-->
 
+<!--SELECTEUR NOMBRE DE JOUEUR·EUSE·S-->
+<h2>Nombre de joueur·euse·s</h2>
+    <div id="player_number_selector">
+      <select name="player_nbr" id="player_nbr" onchange="playerSelector()">
+        <?php
+          // Boucle pour générer le nombre de joueur·euse·s
 
+          $nbr_joueur_min = 2;
+          $nbr_joueur_max = 8;
+          for ($i = $nbr_joueur_min; $i <= $nbr_joueur_max; $i++) {
+            echo "<option value='" . $i . "'>" . $i . "</option>";
+          }
+          ?>
+      </select>
+    </div>
 
+<!--SELECTEUR RACES DES JOUEUR·EUSE·S-->
+<h2>Noms et races jouées</h2>
+    <div id="player_chara">
 
-      
+      <div id="player1">
+        <input id="player1_name" type="text" value="Joueur 1">
+        <select id="player1_race">
+          <?php
+              foreach($liste_races as $option) {
+                echo '<option value="' . $option . '">' . $option . '</option>';
+              }
+            ?>
+        </select>
+      </div>
+
+      <div id="player2">
+        <input id="player2_name" type="text" value="Joueur 2">
+        <select id="player2_race">
+          <?php
+              foreach($liste_races as $option) {
+                echo '<option value="' . $option . '">' . $option . '</option>';
+              }
+            ?>
+        </select>
+      </div>
+
+    </div>
+    
   </body>
 </html>
